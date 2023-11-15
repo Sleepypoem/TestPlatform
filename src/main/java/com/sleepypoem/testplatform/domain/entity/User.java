621a61c1b9e48397ae7198878262a64e23633c2b
Alexander <gallade2014@gmail.com>
2023-11-14 21:57:59 -0600

    Added a base user class entity.

---
+++ src/main/java/com/sleepypoem/testplatform/domain/entity/User.java
@@ -0,0 +1,18 @@
+package com.sleepypoem.testplatform.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class User extends EntityWithTimestamps{
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+}
