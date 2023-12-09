a192ff27c43e8d04a599621facc4bf41f9cd7f21
Alexander <gallade2014@gmail.com>
2023-12-08 22:42:06 -0600

    Added an exception for internal errors.

---
+++ src/main/java/com/sleepypoem/testplatform/exception/MyInternalException.java
@@ -0,0 +1,8 @@
+package com.sleepypoem.testplatform.exception;
+
+public class MyInternalException extends RuntimeException {
+
+    public MyInternalException(String message) {
+        super(message);
+    }
+}
