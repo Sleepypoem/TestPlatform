8d66b17bd1c6fac1ad6d65f99f41b26dcc34dd30
Alexander <gallade2014@gmail.com>
2023-12-09 16:31:00 -0600

    Fixed database tables thanks to the feedback.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,10 +9,10 @@ CREATE TABLE IF NOT EXISTS `test-platform`.`roles` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `description` VARCHAR(255) NULL,
-  `created_at` DATETIME(6) NOT NULL,
-  `updated_at` DATETIME(6) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB ROW_FORMAT = COMPRESSED;
 
 -- -----------------------------------------------------
 -- Table `test-platform`.`images`
@@ -25,27 +25,28 @@ CREATE TABLE IF NOT EXISTS `test-platform`.`images` (
   `width` INT NOT NULL,
   `height` INT NOT NULL,
   `path` VARCHAR(1000) NOT NULL,
-  `created_at` DATETIME(6) NOT NULL,
-  `updated_at` DATETIME(6) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-  `description` VARCHAR(255) NULL,
+ENGINE = InnoDB ROW_FORMAT = COMPRESSED;
 
 -- -----------------------------------------------------
 -- Table `test-platform`.`teachers`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test-platform`.`teachers` (
+CREATE TABLE teachers (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
-   `last_name` VARCHAR(45) NOT NULL,
-  `teacher_code` BIGINT NOT NULL,
-  `image_id` BIGINT NOT NULL,
-  `created_at` DATETIME(6) NOT NULL,
-  `updated_at` DATETIME(6) NOT NULL,
-   PRIMARY KEY (`id`),
-   CONSTRAINT `fk_teachers_images`
-   FOREIGN KEY (`image_id`) REFERENCES `images` (`id`))
-   ENGINE = InnoDB;
+  `last_name` VARCHAR(45) NOT NULL,
+  `teacher_code` BIGINT NOT NULL UNIQUE,
+  `image_id` BIGINT,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  PRIMARY KEY (id),
+  INDEX (first_name),
+  INDEX (last_name),
+  INDEX (teacher_code),
+  CONSTRAINT `fk_teachers_images` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`)
+) ENGINE=InnoDB ROW_FORMAT=COMPRESSED;
 
 -- -----------------------------------------------------
 -- Table `test-platform`.`students`
@@ -53,14 +54,18 @@ CREATE TABLE IF NOT EXISTS `test-platform`.`teachers` (
 CREATE TABLE IF NOT EXISTS `test-platform`.`students` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
-   `last_name` VARCHAR(45) NOT NULL,
-  `student_code` BIGINT NOT NULL,
-  `created_at` DATETIME(6) NOT NULL,
-  `updated_at` DATETIME(6) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `image_id` BIGINT ,
+  `student_code` BIGINT NOT NULL UNIQUE,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
    PRIMARY KEY (`id`),
+   INDEX (first_name),
+   INDEX (last_name),
+   INDEX (student_code),
    CONSTRAINT `fk_students_images`
    FOREIGN KEY (`image_id`) REFERENCES `images` (`id`))
-   ENGINE = InnoDB;
+   ENGINE = InnoDB ROW_FORMAT = COMPRESSED;
 
  -- -----------------------------------------------------
  -- Table `test-platform`.`subjects`
@@ -69,10 +74,10 @@ CREATE TABLE IF NOT EXISTS `test-platform`.`students` (
    `id` BIGINT NOT NULL AUTO_INCREMENT,
    `name` VARCHAR(45) NOT NULL,
    `description` VARCHAR(255) NULL,
-   `created_at` DATETIME(6) NOT NULL,
-   `updated_at` DATETIME(6) NOT NULL,
+   `created_at` DATETIME NOT NULL,
+   `updated_at` DATETIME NOT NULL,
     PRIMARY KEY (`id`))
-    ENGINE = InnoDB;
+    ENGINE = InnoDB ROW_FORMAT = COMPRESSED;
 
 -- -----------------------------------------------------
 -- Table `test-platform`.`tests`
@@ -83,14 +88,14 @@ CREATE TABLE IF NOT EXISTS `test-platform`.`tests` (
    `subject_id` BIGINT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `content` LONGTEXT NULL,
-  `created_at` DATETIME(6) NOT NULL,
-   `updated_at` DATETIME(6) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+   `updated_at` DATETIME NOT NULL,
    PRIMARY KEY (`id`),
    CONSTRAINT `fk_tests_teachers`
    FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`),
    CONSTRAINT  `fk_tests_subjects`
    FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`))
-   ENGINE = InnoDB;
+   ENGINE = InnoDB ROW_FORMAT = COMPRESSED;
 
 -- -----------------------------------------------------
 -- Table `test-platform`.`student_test`
@@ -102,14 +107,14 @@ CREATE TABLE IF NOT EXISTS `test-platform`.`student_tests` (
   `answers` LONGTEXT NULL,
    `score` BIGINT NOT NULL,
   `status` VARCHAR(45) NOT NULL,
-  `created_at` DATETIME(6) NOT NULL,
-   `updated_at` DATETIME(6) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+   `updated_at` DATETIME NOT NULL,
    PRIMARY KEY (`id`),
    CONSTRAINT `fk_student_tests_students`
    FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
    CONSTRAINT `fk_student_tests_tests`
    FOREIGN KEY (`test_id`) REFERENCES `tests` (`id`))
-   ENGINE = InnoDB;
+   ENGINE = InnoDB ROW_FORMAT = COMPRESSED;
 
 -- -----------------------------------------------------
 -- Table `test-platform`.`roles_teachers`
@@ -121,4 +126,4 @@ CREATE TABLE IF NOT EXISTS `test-platform`.`roles_teachers` (
    FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`),
    CONSTRAINT `fk_roles_teachers_roles`
    FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`))
-   ENGINE = InnoDB;
+   ENGINE = InnoDB ROW_FORMAT = COMPRESSED;
