a132f82180f67839d3e775d6261032c4344cd606
Alexander <gallade2014@gmail.com>
2023-12-09 16:18:32 -0600

    Created a DTO for teacher.

---
+++ src/main/java/com/sleepypoem/testplatform/domain/dto/TeacherDto.java
@@ -0,0 +1,22 @@
+package com.sleepypoem.testplatform.domain.dto;
+
+import com.sleepypoem.testplatform.domain.entity.Image;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TeacherDto extends DtoWithTimestamps implements BaseDto<Long> {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Long teacherCode;
+
+    private String roles;
+
+    private Image image;
+}
