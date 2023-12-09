7ef1cef8f0483f46352116c2374e1c78bee3d1bf
Alexander <gallade2014@gmail.com>
2023-12-09 16:17:17 -0600

    Made Teacher entity implement the WithDto interface.

--- src/main/java/com/sleepypoem/testplatform/domain/entity/Teacher.java
+++ src/main/java/com/sleepypoem/testplatform/domain/entity/Teacher.java
@@ -1,19 +1,22 @@
 package com.sleepypoem.testplatform.domain.entity;
 
+import com.sleepypoem.testplatform.config.MapperProvider;
+import com.sleepypoem.testplatform.domain.dto.TeacherDto;
 import com.sleepypoem.testplatform.domain.entity.base.BaseEntity;
+import com.sleepypoem.testplatform.domain.entity.base.EntityWithDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import jakarta.persistence.FetchType;
-
 import java.util.Set;
+
+import static com.sleepypoem.testplatform.constants.ResourceConstants.TEACHER_ROLES_PREFIX;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "teachers")
-public class Teacher extends User implements BaseEntity<Long> {
+public class Teacher extends User implements BaseEntity<Long>, EntityWithDto<TeacherDto> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,10 +28,17 @@ public class Teacher extends User implements BaseEntity<Long> {
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "roles_teachers",
             joinColumns = @JoinColumn(name = "teacher_id"),
-    inverseJoinColumns = @JoinColumn(name = "role_id"))
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
     @OneToOne
-    @JoinColumn(name="image_id")
+    @JoinColumn(name = "image_id")
     private Image image;
+
+    @Override
+    public TeacherDto toDto() {
+        TeacherDto teacherDto = MapperProvider.getMapper().map(this, TeacherDto.class);
+        teacherDto.setRoles(TEACHER_ROLES_PREFIX + id);
+        return teacherDto;
+    }
 }
