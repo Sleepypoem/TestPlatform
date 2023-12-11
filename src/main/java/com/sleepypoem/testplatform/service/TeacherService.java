62b3e995ca9dd26f151fcf23858de8389bd0b203
Alexander <gallade2014@gmail.com>
2023-12-11 12:03:44 -0600

    Fixed bug in annotation.

--- src/main/java/com/sleepypoem/testplatform/service/TeacherService.java
+++ src/main/java/com/sleepypoem/testplatform/service/TeacherService.java
@@ -3,9 +3,9 @@ package com.sleepypoem.testplatform.service;
 import com.sleepypoem.testplatform.domain.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
-@Repository
+@Service
 public class TeacherService extends AbstractQueryableService<Long, Teacher> {
     public TeacherService(JpaRepository<Teacher, Long> repository, JpaSpecificationExecutor<Teacher> specificationExecutor) {
         super(repository, specificationExecutor);
