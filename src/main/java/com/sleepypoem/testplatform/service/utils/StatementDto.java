cab8ddf7f19e13d1ef62bd3ae94df0adfbb07419
Alexander <gallade2014@gmail.com>
2023-12-17 11:00:23 -0600

    Removed unnecessary code.

--- src/main/java/com/sleepypoem/testplatform/service/utils/StatementDto.java
+++ src/main/java/com/sleepypoem/testplatform/service/utils/StatementDto.java
@@ -1,11 +1,9 @@
 package com.sleepypoem.testplatform.service.utils;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class StatementDto {
 
