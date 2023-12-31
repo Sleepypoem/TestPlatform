cd5f6aff74b17732cb098906c23974e77783099f
Alexander <gallade2014@gmail.com>
2023-12-31 01:07:00 -0600

    Added a validation logic to the abstract service class.

--- src/main/java/com/sleepypoem/testplatform/service/AbstractService.java
+++ src/main/java/com/sleepypoem/testplatform/service/AbstractService.java
@@ -2,19 +2,30 @@ package com.sleepypoem.testplatform.service;
 
 import com.google.common.base.Preconditions;
 import com.sleepypoem.testplatform.domain.entity.base.BaseEntity;
+import com.sleepypoem.testplatform.exception.MyValidationException;
 import com.sleepypoem.testplatform.service.base.Service;
+import com.sleepypoem.testplatform.service.validation.DefaultValidator;
+import com.sleepypoem.testplatform.service.validation.IValidator;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Map;
 import java.util.Objects;
 
+@Setter
+@Getter
 public class AbstractService<ID, E extends BaseEntity<ID>> extends AbstractReadOnlyService<ID, E> implements Service<ID, E> {
+    protected IValidator<E> validator;
     public AbstractService(JpaRepository<E, ID> repository) {
         super(repository);
+        validator = new DefaultValidator<>();
     }
 
     @Override
     public E create(E entity) {
         checkNotNullEntity(entity);
+        validate(entity);
         return repository.save(entity);
     }
 
@@ -23,6 +34,7 @@ public class AbstractService<ID, E extends BaseEntity<ID>> extends AbstractReadO
         checkNotNullEntity(entity);
         Preconditions.checkNotNull(id, "URI id must not be null");
         Preconditions.checkArgument(Objects.equals(entity.getId(), id), "URI id and entity id must be equal");
+        validate(entity);
         return repository.saveAndFlush(entity);
     }
 
@@ -38,5 +50,12 @@ public class AbstractService<ID, E extends BaseEntity<ID>> extends AbstractReadO
 
     private void checkNotNullEntity(E entity) {
         Preconditions.checkNotNull(entity, "Entity must not be null");
+    }
+
+    private void validate(E entity) {
+        Map<String, String> errors = getValidator().isValid(entity);
+        if (!errors.isEmpty()) {
+            throw new MyValidationException(errors);
+        }
     }
 }
