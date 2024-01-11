61ec2b56b8419e249057c63bc03973525ee93141
Alexander <gallade2014@gmail.com>
2024-01-11 11:33:54 -0600

    Added handler for validation exception.

--- src/main/java/com/sleepypoem/testplatform/config/GlobalExceptionHandler.java
+++ src/main/java/com/sleepypoem/testplatform/config/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sleepypoem.testplatform.config;
 
 
 import com.sleepypoem.testplatform.exception.MyInternalException;
+import com.sleepypoem.testplatform.exception.MyValidationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MyInternalException.class)
     public ResponseEntity<Object> handleMyInternalException(MyInternalException ex, WebRequest request) {
         String bodyOfResponse = "Semantic error: " + ex.getMessage();
+
+        return createResponseEntity(bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(MyValidationException.class)
+    public ResponseEntity<Object> handleMyValidationException(MyValidationException ex, WebRequest request) {
+        String bodyOfResponse = "Validation error: " + ex.getMessage();
 
         return createResponseEntity(bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
